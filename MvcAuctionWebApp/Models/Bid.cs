33f3ec854c6c7cada445005a8a72a8d1d161d5fc
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-12-16 13:38:48 +0100

    Modified some functions.

---
+++ MvcAuctionWebApp/Models/Bid.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcAuctionWebApp.Models
+{
+    public class Bid
+    {
+        public Bid()
+        {
+            Timestamp = DateTime.Now;
+        }
+
+        public long Id { get; set; }
+
+        public long AuctionId { get; set; }
+
+        [Required]
+        public DateTime Timestamp { get; set; }
+
+        public string Username { get; set; }
+
+        [Range(1, double.MaxValue)]
+        public decimal Amount { get; set; }
+    }
+}
